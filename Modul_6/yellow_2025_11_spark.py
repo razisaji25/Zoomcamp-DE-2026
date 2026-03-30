ff48d0b50e093213f22a5d0040c8404bbae0c201
Ubuntu <ubuntu@localhost.localdomain>
2026-03-30 09:30:46 +0800

    update all

--- Modul_6/yellow_2025_11_spark.py
+++ Modul_6/yellow_2025_11_spark.py
@@ -10,7 +10,7 @@ df = spark.read.parquet("yellow_tripdata_2025-11.parquet")
 
 # repartition
 df_4 = df.repartition(4)
-
+uv run python yellow_2025_11_spark.py
 # write parquet
 output_path = "yellow_2025_11_repartitioned"
 df_4.write.mode("overwrite").parquet(output_path)
